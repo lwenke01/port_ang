947e6063dc53affde104065e78e712bdfaf975a4
U-2D\lisa.wenke <lisa.wenke@32626-E7470.2d.hq>
2018-03-27 09:32:34 -0800

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCardModule, MatToolbarModule, MatMenuModule,MatIconModule, MatTabsModule  } from '@angular/material';
 
 
 import { AppComponent } from './app.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsComponent,
+    NavComponent,
+    FooterComponent,
+    HeaderComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+     MatMenuModule,
+ MatToolbarModule,
+MatCardModule,
+MatIconModule,
+MatTabsModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
